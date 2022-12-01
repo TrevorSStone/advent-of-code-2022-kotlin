ffc18d17b372b8f8f19ecfff10aed2d16bce9df7
Trevor Stone <trevor.stone@ww.com>
2022-12-01 18:58:18 -0500

    Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    var maxCalories = 0
+    (input + "end").runningFold(0) { currentElfCalories, calLine ->
+      calLine.toIntOrNull()?.let { currentElfCalories + it }
+          ?: run {
+            if (currentElfCalories > maxCalories) {
+              maxCalories = currentElfCalories
+            }
+            0
+          }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    return maxCalories
+  }
+
+  fun part2(input: List<String>): Int {
+    var topCalories = 0
+    var secondCalories = 0
+    var thirdCalories = 0
+    (input + "end").runningFold(0) { currentElfCalories, calLine ->
+      calLine.toIntOrNull()?.let { currentElfCalories + it }
+          ?: run {
+            when {
+              currentElfCalories >= topCalories -> {
+                thirdCalories = secondCalories
+                secondCalories = topCalories
+                topCalories = currentElfCalories
+              }
+              currentElfCalories >= secondCalories -> {
+                thirdCalories = secondCalories
+                secondCalories = currentElfCalories
+              }
+              currentElfCalories >= thirdCalories -> {
+                thirdCalories = currentElfCalories
+              }
+            }
+
+            0
+          }
     }
+    return topCalories + secondCalories + thirdCalories
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 24000)
+  check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("Day01")
+  println(part1(input))
+  println(part2(input))
 }
