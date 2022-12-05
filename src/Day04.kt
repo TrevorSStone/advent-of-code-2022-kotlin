5d9a856c30b51a7034db994fd350dafb4553ca91
Trevor Stone <trevor.stone@ww.com>
2022-12-05 00:03:16 -0500

    Solve Day04

---
+++ src/Day04.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+  fun part1(input: List<String>): Int =
+      input
+          .asSequence()
+          .flatMap { line -> line.split('-', ',') }
+          .chunked(2)
+          .map { (first, second) -> first.toInt()..second.toInt() }
+          .chunked(2)
+          .map { (first, second) ->
+            first.intersect(second).let { it.size == first.count() || it.size == second.count() }
+          }
+          .count { it }
+
+  fun part2(input: List<String>): Int =
+      input
+          .asSequence()
+          .flatMap { line -> line.split('-', ',') }
+          .chunked(2)
+          .map { (first, second) -> first.toInt()..second.toInt() }
+          .chunked(2)
+          .map { (first, second) -> first.intersect(second).isNotEmpty() }
+          .count { it }
+
+  val testInput = readInput("Day04_test")
+  check(part1(testInput)== 2)
+  check(part2(testInput) == 4)
+  //
+  val input = readInput("Day04")
+  println(part1(input))
+  println(part2(input))
+}
